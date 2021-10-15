73e76e570c2be41ca9bdb68f108044272a6ca6b9
george-shammar <george.gbenle@outlook.com>
2021-10-15 21:10:12 +0000

    Create contract

--- contracts/DeFinse.sol
+++ contracts/DeFinse.sol
@@ -5,3 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract DeFinse is Ownable, ERC20, Pausable {
+    
+}
